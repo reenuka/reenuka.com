e49f5fd84aaad835931b845d94e4078404bb53ae
Reenuka Sarika Rāmphal <reenuka@gmail.com>
2018-11-02 15:10:51 -0700

    added flexbox

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
+import { Router, Switch, Route } from "react-router-dom";
+
 import './app.css';
 import Lobby from './Lobby';
+import Intro from './Intro';
+import Summary from './Summary';
+import Blog from './Blog';
+import Portfolio from './Portfolio';
 
 class App extends Component {
   render() {
@@ -8,6 +14,15 @@ class App extends Component {
       <div>
         Hi, it's me, Reenuka.
         <Lobby />
+        {/* <Router>
+          <Switch>
+            <Route exact path="/" component={Lobby} />
+            <Route path="/intro" component={Intro} />
+            <Route path="/summary" component={Summary} />
+            <Route path="/blog" component={Blog} />
+            <Route path="/portfolio" component={Portfolio} />
+          </Switch>
+        </Router> */}
       </div>
     );
   }
--- src/Footer.js
+++ src/Footer.js
@@ -2,15 +2,60 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Footer = (props) => {
-    const StyledFooter = styled.div`
+    const StyledFooter = styled.div` 
         color: purple;
         font-size: 36px;
+        text-align: center;
+
+        display: flex;
+        flex-direction: column;
+        justify-content: space-around;
+
+        width: 1000px;
+        height: 200px;
+        border: 5px solid #8AC007;
+
+        .row {
+            display: flex;
+            flex-direction: row;
+            justify-content: space-between;
+        }
+
+        .left {
+            color: pink;
+            text-align: left;
+        }
+        
+        .right {
+            color: yellow;
+            text-align: right;
+        }
+        
+        .copyright {
+            color: lavender;
+            font-size: 10px;
+        }
     `
 
     return (
         <div>
-            <StyledFooter>
-                Footer
+            <StyledFooter className="test">
+                <div>
+                    Footer Navigation
+                </div>
+
+                <div className="row">
+                    <div className="left">
+                        Contact
+                    </div>
+                    <div className="right">
+                        Something?
+                    </div>
+                </div>
+
+                <div className="copyright">
+                    COPYRIGHT © 2018 - REENUKA SARIKA RĀMPHAL
+                </div>
             </StyledFooter>
         </div>
     );
